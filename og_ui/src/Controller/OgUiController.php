70c6139433424ee226026f57e80385445d4db3a4
Dan Braghis <dan.braghis@torchbox.com>
2017-03-10 18:13:26 +0000

    Linting

--- og_ui/src/Controller/OgUiController.php
+++ og_ui/src/Controller/OgUiController.php
@@ -6,11 +6,9 @@ use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Link;
-use Drupal\og\GroupManager;
 use Drupal\og\Og;
 use Drupal\og\GroupTypeManager;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
@@ -153,16 +151,14 @@ class OgUiController extends ControllerBase {
       // Add the edit role link if the role is editable.
       if (!$role->isLocked()) {
         $operations = [];
-        // if ($role->access('update') && $role->hasLinkTemplate('edit-form')) {
-        if ($role->hasLinkTemplate('edit-form')) {
+        if ($role->access('update') && $role->hasLinkTemplate('edit-form')) {
           $operations['edit'] = [
             'title' => $this->t('Edit role'),
             'weight' => 10,
             'url' => $role->urlInfo('edit-form'),
           ];
         }
-        //if ($role->access('delete') && $role->hasLinkTemplate('delete-form')) {
-        if ($role->hasLinkTemplate('delete-form')) {
+        if ($role->access('delete') && $role->hasLinkTemplate('delete-form')) {
           $operations['delete'] = [
             'title' => $this->t('Delete role'),
             'weight' => 100,
@@ -214,6 +210,7 @@ class OgUiController extends ControllerBase {
    *   The group bundle.
    *
    * @return \Drupal\Core\StringTranslation\TranslatableMarkup
+   *   The roles overview page title.
    */
   public function rolesOverviewPageTitleCallback($entity_type, $bundle) {
     return $this->t('OG @type - @bundle roles', [
